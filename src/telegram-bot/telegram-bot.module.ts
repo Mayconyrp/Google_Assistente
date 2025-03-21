0ca458435f7dffed95e7a44ac539f71be3ce63db
mayconyrp <csa.devmaycon@gmail.com>
2025-03-21 10:59:20 -0300

    Resolvido: Problema com Telegram_Module

---
+++ src/telegram-bot/telegram-bot.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegramBotService } from './telegram-bot.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TelegrafModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        token: configService.get<string>('BOT_TOKEN'),
+      }),
+      inject: [ConfigService],
+    }),
+    HttpModule,
+  ],
+  providers: [TelegramBotService],
+  controllers: [],
+})
+export class TelegramBotModule {}
